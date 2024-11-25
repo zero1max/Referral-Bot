cbccdea064267437bd1778eacd3662d52dbde436
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-11-25 19:12:16 +0500

    Update Commit

--- handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Dispatcher
-from handlers import start
-
-
-def register_all_handlers(dp: Dispatcher):
-    dp.include_router(start.router)
-
--- middlewares/logging_middeware.py
+++
@@ -1,7 +0,0 @@
-from aiogram import BaseMiddleware
-
-
-class CustomLoggingMiddleware(BaseMiddleware):
-    async def __call__(self, handler, event, data):
-        print(f"Logging event: {event}")
-        return await handler(event, data)
